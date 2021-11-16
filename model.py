77bf4e9e5546809f89e576a3afc6934c2212fd88
AJ Auntor <auntorknocks@gmail.com>
2021-11-16 22:06:11 +0600

    model

---
+++ model.py
@@ -0,0 +1,11 @@
+
+
+    def __repr__(self):
+        identity = inspect(self).identity
+
+        if identity is None:
+            pk = f"(transient {id(self)})"
+        else:
+            pk = ", ".join(str(value) for value in identity)
+
+        return f"<{type(self).__name__} {pk}>"
