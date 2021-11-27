ba945a79c0c3c65ceb819369026dd5bd662aadb6
AJ Auntor <auntorknocks@gmail.com>
2021-11-27 19:16:07 +0600

    unclear sort and get

--- test_basic_app.py
+++ test_basic_app.py
@@ -71,13 +71,7 @@ def test_sqlite_relative_path(app, tmp_path):
     app.instance_path = tmp_path / "instance"
 
     # tests default to memory, shouldn't create
-    SQLAlchemy(app).get_engine()
-    assert not app.instance_path.exists()
-
-    # absolute path, shouldn't create
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.sqlite"
-    SQLAlchemy(app).get_engine()
-    assert not app.instance_path.exists()
+   
 
     # relative path, should create
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.sqlite"
